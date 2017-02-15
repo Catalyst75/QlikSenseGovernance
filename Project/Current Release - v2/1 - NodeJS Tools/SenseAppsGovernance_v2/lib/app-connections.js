e5752223b83eae7689324b900024d256e8866e85
Renato Vieira <rvieiradossantos@gmail.com>
2017-02-15 12:38:36 +0000

    App-Connections fix for 3.2
    Removed a deprecated method - GetProperties - that would hang the
    connections information retrieval.

--- Project/Current Release - v2/1 - NodeJS Tools/SenseAppsGovernance_v2/lib/app-connections.js
+++ Project/Current Release - v2/1 - NodeJS Tools/SenseAppsGovernance_v2/lib/app-connections.js
@@ -23,35 +23,33 @@ module.exports={
       }).then(function(documentId){
         //Asking for the available connections of the document
         global.openDoc(documentId,"","","",conn_data.no_data).then(function(app){
-          app.getProperties().then(function(reply) {
-            app.getConnections().then(function(document_connections){
-              var received_time = Date.now();
-              console.log("It took "+ (received_time-start_time) +"ms to receive the info.");
+          app.getConnections().then(function(document_connections){
+            var received_time = Date.now();
+            console.log("It took "+ (received_time-start_time) +"ms to receive the info.");
 
-              var loading_time = 0;
-              if(conn_data.timer_mode)
-                loading_time=received_time-start_time;
+            var loading_time = 0;
+            if(conn_data.timer_mode)
+              loading_time=received_time-start_time;
 
-              //Setting up data and options for XML file storage
-              var data = {
-                document_connections,
-                qsLoadingTime: loading_time
-              };
+            //Setting up data and options for XML file storage
+            var data = {
+              document_connections,
+              qsLoadingTime: loading_time
+            };
 
-              var options = {
-                useCDATA: true
-              }
+            var options = {
+              useCDATA: true
+            }
 
-              var xml_doc_connections = js2xmlparser.parse("documentsConnections", data, options);
+            var xml_doc_connections = js2xmlparser.parse("documentsConnections", data, options);
 
-              //Storing XML with Connections List
-              fs.writeFile('AppStructures/DocumentsConnections_'+conn_data.user_directory + '_' + conn_data.user_name+'.xml', xml_doc_connections, function(err) {
-                if (err) throw err;
-                console.log('DocumentsConnections.xml file saved');
-                resolve("Checkpoint: Connections List is loaded");
-              });
+            //Storing XML with Connections List
+            fs.writeFile('AppStructures/DocumentsConnections_'+conn_data.user_directory + '_' + conn_data.user_name+'.xml', xml_doc_connections, function(err) {
+              if (err) throw err;
+              console.log('DocumentsConnections.xml file saved');
+              resolve("Checkpoint: Connections List is loaded");
             });
-          })
+          });
         })
       })
     }); //promise
